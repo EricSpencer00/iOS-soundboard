9196cd12a6ab11d8d61a20d8cecfa9cf428bdbb4
EricSpencer00 <espencer2@luc.edu>
2025-08-10 16:20:32 -0500

    Initial Commit

---
+++ Soundboard/SoundboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundboardApp.swift
+//  Soundboard
+//
+//  Created by Eric Spencer on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SoundboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
